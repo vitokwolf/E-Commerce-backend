ed0ed7515b5ddea5198bb15f9e2963580c40ffea
Victor Lupascu <sinajeen@gmail.com>
2021-07-30 23:37:45 -0500

    add product delete route

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -122,6 +122,22 @@ router.put('/:id', (req, res) => {
 
 router.delete('/:id', (req, res) => {
   // delete one product by its `id` value
+  Product.destroy({
+    where: {
+      id: req.params.id
+    }
+  })
+    .then(dbData => {
+      if (!dbData) {
+        res.status(404).json({ message: 'No product found with this id' });
+        return;
+      }
+      res.json(dbData);
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
 });
 
 module.exports = router;
