75eb26c93ee7fe9d2ea516ca69f0ead4143779a9
Victor Lupascu <sinajeen@gmail.com>
2021-07-30 21:11:58 -0500

    add relations

--- models/index.js
+++ models/index.js
@@ -3,14 +3,21 @@ const Product = require('./Product');
 const Category = require('./Category');
 const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
+const { belongsTo } = require('./Product');
 
 // Products belongsTo Category
-
+Product.belongsTo(Category);
 // Categories have many Products
-
+Category.hasMany(Product);
 // Products belongToMany Tags (through ProductTag)
-
+Product.belongsToMany(Tag, {
+  through: ProductTag
+});
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, {
+  through: ProductTag
+});
+
 
 module.exports = {
   Product,
