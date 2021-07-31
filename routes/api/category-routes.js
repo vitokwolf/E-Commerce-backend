87f427a45ec19abeb0d0823d2b6c7d697df96e1b
Victor Lupascu <sinajeen@gmail.com>
2021-07-30 21:55:21 -0500

    add category get routes

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -5,12 +5,37 @@ const { Category, Product } = require('../../models');
 
 router.get('/', (req, res) => {
   // find all categories
-  // be sure to include its associated Products
+  Category.findAll({
+    include: [
+      {
+        model: Product
+      }
+    ]
+  })
+    .then(data => res.json(data))
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
 });
 
 router.get('/:id', (req, res) => {
   // find one category by its `id` value
-  // be sure to include its associated Products
+  Category.findOne({
+    include: [
+      {
+        model: Product
+      }
+    ],
+    where: {
+      id: req.params.id
+    }
+  })
+    .then(data => res.json(data))
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
 });
 
 router.post('/', (req, res) => {
